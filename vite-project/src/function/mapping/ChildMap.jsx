88b7776a9b424d80f3db8827d022461f18b7c544
Yash-D-Wanth <yashjaiganesh@gmail.com>
2026-05-26 13:12:09 +0530

    mapping

--- vite-project/src/function/mapping/ChildMap.jsx
+++ vite-project/src/function/mapping/ChildMap.jsx
@@ -1,7 +1,9 @@
 export default function ChildMap({person}){
     return(
         <>
-        <li>i am {person.name},i am {person.role} of straw hats and many of them call me {person.nickname}</li>
+        <li>i am {person.name},i am {person.role} of straw hats and many of them call me {person.nickname} my opponents are {person.opponents.map(opponent=>(
+            <p key={opponent}>{opponent}</p>
+        ))}</li>
         </>
     )
 }
--- vite-project/src/function/mapping/ParentMap.jsx
+++ vite-project/src/function/mapping/ParentMap.jsx
@@ -6,13 +6,15 @@ export default function ParentMap(){
             id:1,
             name:"yashwanth",
             role:"captain",
-            nickname:"dark-flame"
+            nickname:"dark-flame",
+            opponents:["black-beard","imu","kaido"]
         },
         {
             id:2,
             name:"nandhini",
             role:"navigator",
-            nickname:"heaven-beauty"
+            nickname:"heaven-beauty",
+            opponents:["big-mom","luccy","caesar"]
         }
     ]
     return(
